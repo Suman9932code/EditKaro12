cb6833ace7e36622c7e5f028951972f75689bc78
Suman9932code <sumanjana77276@gmail.com>
2024-10-11 12:37:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      window.scrollTo({
-        top: target.offsetTop,
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: 'smooth'
     });
   });
-  
-  // Animation on form submit
-  document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Your message has been sent!');
-  });
+});
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Your message has been sent!');
+});
   
